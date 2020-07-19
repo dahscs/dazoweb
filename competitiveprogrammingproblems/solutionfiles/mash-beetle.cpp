e95f280e7b4505c3a3976a9074c038f09d2e3276
Evan Partidas <evanpartidas@gmail.com>
2020-07-19 09:02:31 -0400

    Reprogram Mash-beetle

--- competitiveprogrammingproblems/solutionfiles/mash-beetle.cpp
+++ competitiveprogrammingproblems/solutionfiles/mash-beetle.cpp
@@ -1,6 +1,8 @@
 #include <iostream>
+#include <vector>
 
 //Author: Evan Partidas
+//NOTE: I re-programmed this solution, inspired by Alberto Alvarez's Solution
 
 using namespace std;
 
@@ -11,55 +13,60 @@ int visited[MAXN][MAXN][4];
 bool board[MAXN][MAXN];
 int dirx[] = {-1,0,1,0};
 int diry[] = {0,1,0,-1};
-/*
- * This is the main logic of the program.
- * For the visited array:
- * A value of 0 means a node is unvisited
- * A value of 1 means a node is currently being relaxed
- * A value of 2 means a node is part of a cycle
- * A value of 3 means a node is not part of a cycle or is the head of a cycle.
- * 
- * Note:
- * I don't recommend coding like this if you are not extremely good
- * with thinking recursively. I coded it like this for reading purposes.
- * When I initially solved this problem I coded it very differently. 
- * There are many other ways to solve this problem but this is the 
- * cleanest code I came up with.
- */
-int calc(int x,int y,int d,int val)
+int calc(int x,int y,int d)
 {
 	if(visited[x][y][d]==1)
 	{
-		dp[x][y][d] = val-dp[x][y][d];//Length of the cycle
-		visited[x][y][d] = 2;//Mark the head of the cycle
 		return dp[x][y][d];
 	}
-	if(visited[x][y][d]>1)
+	int val = 0;
+	vector<int> xpath,ypath,dpath;
+	while(!visited[x][y][d])
 	{
-		visited[x][y][d]=3;//This is for the backtrack if statement
-		return dp[x][y][d]+min(val,1);//calc(x,y,d,0) should not get a bonus 1 for no reason.
+		xpath.push_back(x);
+		ypath.push_back(y);
+		dpath.push_back(d);
+		visited[x][y][d]=1;
+		dp[x][y][d] = val++;
+		int nx = x+dirx[d];
+		int ny = y+diry[d];
+		int nd = d;
+		if(!board[nx][ny])
+		{
+			nx = x;
+			ny = y;
+			nd = (d+1)%4;
+		}
+		x = nx;
+		y = ny;
+		d = nd;
 	}
-	int ret = 0;
-	int nx=x+dirx[d];
-	int ny=y+diry[d];
-	int nd = d;
-	if(!board[nx][ny])
+	int cyclelen;
+	int i=xpath.size()-1;
+	if(visited[x][y][d]==1)
 	{
-		nx = x;
-		ny = y;
-		nd = (d+1)%4;
+		cyclelen = val-dp[x][y][d];
+		for(;i>=0;i--)
+		{
+			int cx = xpath[i];
+			int cy = ypath[i];
+			int cd = dpath[i];
+			dp[cx][cy][cd] = cyclelen;
+			visited[cx][cy][cd] = 2;
+			if(cx==x&&cy==y&&cd==d) break;
+		}
+		i--;
 	}
-	visited[x][y][d] = 1;
-	dp[x][y][d] = val;
-	ret = calc(nx,ny,nd,val+1);
-	dp[x][y][d] = ret;
-	if(visited[x][y][d]==2||visited[nx][ny][nd]==3)
-	{//As I backtrack, I increment the value by 1 (this is for nodes outside the cycle)
-		visited[x][y][d]=3;
-		ret++;
+	else cyclelen = dp[x][y][d]; //visited[x][y][d]==2
+	for(;i>=0;i--)
+	{
+		int cx = xpath[i];
+		int cy = ypath[i];
+		int cd = dpath[i];
+		visited[cx][cy][cd]=2;
+		dp[cx][cy][cd] = ++cyclelen;
 	}
-	else visited[x][y][d] = 2;
-	return ret;
+	return cyclelen;
 }
 
 int main()
@@ -85,7 +92,7 @@ int main()
 				for(int d=0;d<4;d++)
 				{
 					
-					if(calc(i,j,d,0)>m)
+					if(calc(i,j,d)>m)
 					{
 						x=i;
 						y=j;
